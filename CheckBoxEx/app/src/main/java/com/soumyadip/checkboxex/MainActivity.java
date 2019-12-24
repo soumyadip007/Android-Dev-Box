9685388c8ccdd05bd600fb8c331382bbfc6c9a76
unknown <soumyadip.note@GMAIL.COM>
2019-12-24 23:47:39 +0530

    CheckBox Android

---
+++ CheckBoxEx/app/src/main/java/com/soumyadip/checkboxex/MainActivity.java
@@ -0,0 +1,53 @@
+package com.soumyadip.checkboxex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    CheckBox pizza,coffe,burger;
+    Button buttonOrder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
+    }
+    public void addListenerOnButtonClick(){
+        //Getting instance of CheckBoxes and Button from the activty_main.xml file
+        pizza=(CheckBox)findViewById(R.id.checkBox);
+        coffe=(CheckBox)findViewById(R.id.checkBox2);
+        burger=(CheckBox)findViewById(R.id.checkBox3);
+        buttonOrder=(Button)findViewById(R.id.button);
+
+        //Applying the Listener on the Button click
+        buttonOrder.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int totalamount=0;
+                StringBuilder result=new StringBuilder();
+                result.append("Selected Items:");
+                if(pizza.isChecked()){
+                    result.append("\nPizza 100Rs");
+                    totalamount+=100;
+                }
+                if(coffe.isChecked()){
+                    result.append("\nCoffe 50Rs");
+                    totalamount+=50;
+                }
+                if(burger.isChecked()){
+                    result.append("\nBurger 120Rs");
+                    totalamount+=120;
+                }
+                result.append("\nTotal: "+totalamount+"Rs");
+                //Displaying the message on the toast
+                Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
