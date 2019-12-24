76193ae7fcb8d1ef682cb6018239cd96821a6c29
unknown <soumyadip.note@GMAIL.COM>
2019-12-24 23:43:30 +0530

    Toggle Button

---
+++ ToggleButtonEx/app/src/main/java/com/example/togglebuttonex/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.togglebuttonex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+    private ToggleButton toggleButton1, toggleButton2;
+    private Button buttonSubmit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick(){
+        //Getting the ToggleButton and Button instance from the layout xml file
+        toggleButton1=(ToggleButton)findViewById(R.id.toggleButton);
+        toggleButton2=(ToggleButton)findViewById(R.id.toggleButton2);
+        buttonSubmit=(Button)findViewById(R.id.button);
+
+        //Performing action on button click
+        buttonSubmit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                StringBuilder result = new StringBuilder();
+                result.append("ToggleButton1 : ").append(toggleButton1.getText());
+                result.append("\nToggleButton2 : ").append(toggleButton2.getText());
+                //Displaying the message in toast
+                makeText(getApplicationContext(), result.toString(), LENGTH_LONG).show();
+            }
+
+        });
+
+    }
+}
