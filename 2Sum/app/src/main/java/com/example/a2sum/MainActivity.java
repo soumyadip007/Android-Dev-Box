8c1a46735b5a365a6777624ff76d9a96af49370b
unknown <soumyadip.note@GMAIL.COM>
2019-12-24 23:06:23 +0530

    Sum Of Two Numbers App

---
+++ 2Sum/app/src/main/java/com/example/a2sum/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a2sum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edittext1;
+    private EditText edittext2;
+    private Button buttonSum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        edittext1 = (EditText) findViewById(R.id.editText1);
+        edittext2 = (EditText) findViewById(R.id.editText2);
+        buttonSum = (Button) findViewById(R.id.button);
+
+        buttonSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value1=edittext1.getText().toString();
+                String value2=edittext2.getText().toString();
+                int a=Integer.parseInt(value1);
+                int b=Integer.parseInt(value2);
+                int sum=a+b;
+                Toast.makeText(getApplicationContext(),String.valueOf(sum), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
