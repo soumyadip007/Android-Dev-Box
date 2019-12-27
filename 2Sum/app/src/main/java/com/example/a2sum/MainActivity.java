9f5f9ee4761758a238651abaa47564179d5d7f74
unknown <soumyadip.note@GMAIL.COM>
2019-12-27 20:11:12 +0530

    Android Config

--- 2Sum/app/src/main/java/com/example/a2sum/MainActivity.java
+++ 2Sum/app/src/main/java/com/example/a2sum/MainActivity.java
@@ -13,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText edittext1;
     private EditText edittext2;
     private Button buttonSum;
+    private Button buttonSub;
 
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -22,10 +23,12 @@ public class MainActivity extends AppCompatActivity {
         addListenerOnButton();
     }
 
+
     public void addListenerOnButton() {
         edittext1 = (EditText) findViewById(R.id.editText1);
         edittext2 = (EditText) findViewById(R.id.editText2);
         buttonSum = (Button) findViewById(R.id.button);
+        buttonSub = (Button) findViewById(R.id.button1);
 
         buttonSum.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,8 +38,23 @@ public class MainActivity extends AppCompatActivity {
                 int a=Integer.parseInt(value1);
                 int b=Integer.parseInt(value2);
                 int sum=a+b;
-                Toast.makeText(getApplicationContext(),String.valueOf(sum), Toast.LENGTH_LONG).show();
+                String s="Sum is"+String.valueOf(sum);
+                Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
             }
         });
-    }
+
+
+    buttonSub.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String value1=edittext1.getText().toString();
+            String value2=edittext2.getText().toString();
+            int a=Integer.parseInt(value1);
+            int b=Integer.parseInt(value2);
+            int sum=a-b;
+            String s="Sum is"+String.valueOf(sum);
+            Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
+        }
+    });
+}
 }
