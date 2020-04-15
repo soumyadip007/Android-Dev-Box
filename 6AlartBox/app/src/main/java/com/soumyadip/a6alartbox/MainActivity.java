60743e9b39d8d8e6bb74d025ec405688f0a8d085
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-04-15 21:37:55 +0530

    Update MainActivity.java

--- 6AlartBox/app/src/main/java/com/soumyadip/a6alartbox/MainActivity.java
+++ 6AlartBox/app/src/main/java/com/soumyadip/a6alartbox/MainActivity.java
@@ -27,23 +27,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //Uncomment the below code to Set the message and title from the strings.xml file
-                builder.setMessage(R.string.dialog_message) .setTitle(R.string.dialog_title);
+                builder.setMessage(R.string.dialog_message)
+                        .setTitle("NIIT");
+
+
 
                 //Setting message manually and performing action on button click
-                builder.setMessage("Do you want to close this application ?")
+                builder.setMessage
+                        ("Do you want to close this application ?")
                         .setCancelable(false)
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
+                        .setPositiveButton("NIIT",
+                                new DialogInterface.OnClickListener() {
+                            public void onClick
+                                    (DialogInterface dialog, int id) {
                                 finish();
-                                Toast.makeText(getApplicationContext(),"you choose yes action for alertbox",
+                                Toast.makeText(getApplicationContext(),
+                                        "you choose yes action" +
+                                                " for alertbox",
                                         Toast.LENGTH_SHORT).show();
                             }
                         })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                        .setNegativeButton("No", new DialogInterface
+                                .OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 //  Action for 'NO' Button
                                 dialog.cancel();
-                                Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+                                finish();
+                                Toast.makeText(getApplicationContext(),
+                                        "you choose no action for alertbox",
                                         Toast.LENGTH_SHORT).show();
                             }
                         });
